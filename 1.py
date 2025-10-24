3b409fa738929f1f65a0036b98c59452acee0394
ei2537 <mabuchi.ei@gmail.com>
2025-10-24 18:41:26 +0900

    Auto Update 2025-10-24 18:41:26

--- 1.py
+++ 1.py
@@ -234,6 +234,7 @@ class PumpkinTalk:
             comma_count = sentence.count("、")
             period_count = sentence.count("。")
             tcomma_count = sentence.count("...")
+            char_count = char_count - comma_count - period_count - tcomma_count
             
             sentence_duration = (
                 char_count * CHAR_DURATION + 
