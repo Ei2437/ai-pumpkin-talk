7ffd432951fd5b6b1c297bf25fd48b51cf409107
ei2537 <mabuchi.ei@gmail.com>
2025-10-26 11:08:58 +0900

    Auto Update 2025-10-26 11:08:58

--- 1.py
+++ 1.py
@@ -19,7 +19,7 @@ import threading
 import wave
 
 # ==== 映像設定 ====
-w, h = 1920, 1020
+w, h = 960, 510
 BG_VIDEO_PATH = "videos/BG.mp4"
 VIDEO_MAIN = "videos/Pumpkin-Center.mov"
 VIDEO_FULL2 = "videos/Pumpkin-Center2Left.mov"
